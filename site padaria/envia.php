39b39d149019a19cb83ae817cedc6a635525732f
cedflores <cedflores07@gmail.com>
2023-06-20 21:41:18 -0300

    inicio do php
    Início do PHP

--- site padaria/envia.php
+++ site padaria/envia.php
@@ -1,2 +1,20 @@
 <?php
 
+$nome = addslashes($_POST['nome']);
+$email = addslashes($_POST['email']);
+$telefone = addslashes($_POST['telefone']);
+
+$para = "cedflores07@gmail.com"
+$assunto = "Coleta de dados - Webdesigner"
+
+$corpo = "Nome" .$nome. "\n". "E-mail:" .$email. "\n" . "Telefone:" .$telefone;
+
+$cabeca = "From: rapsilvaflores@gmail.com". "\n". "Replay-to:" .$email. "\n". "X=Mailer:PHP/".phpversion(); 
+
+if(mail($para,$assunto,$corpo,$cabeca)){
+    echo("E-mail enviado com sucesso!")
+} else {
+    echo("Houve um erro ao enviar o email");
+}
+
+?>
